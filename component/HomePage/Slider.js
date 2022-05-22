8a61035a920e96513b5bdd32ee9a37c7d6518d77
dahalprastut <dahalprastut@gmail.com>
2022-05-22 23:28:01 +0545

    slider fixed

--- component/HomePage/Slider.js
+++ component/HomePage/Slider.js
@@ -1,9 +1,9 @@
-import React, { Component, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+} from "react";
 import Slider from "react-slick";
-import {
-  motion,
-  useAnimation,
-} from "framer-motion";
 import useMousePosition from "../../utils/useMousePosition";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -12,28 +12,15 @@ export default function VerticalMode() {
   const hoverdState = useRef(null);
   const settings = {
     dots: false,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 5000,
     //   adaptiveHeight: true,
     infinite: true,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
     vertical: true,
     verticalSwiping: false,
     arrows: false,
-    //   beforeChange: function (
-    //     currentSlide,
-    //     nextSlide
-    //   ) {
-    //     console.log(
-    //       "before change",
-    //       currentSlide,
-    //       nextSlide
-    //     );
-    //   },
-    //   afterChange: function (currentSlide) {
-    //     console.log("after change", currentSlide);
-    //   },
   };
   const { mouseX, mouseY } = useMousePosition();
   const onHover = () => {
@@ -42,13 +29,19 @@ export default function VerticalMode() {
   const onLeave = () => {
     hoverdState.current.style.display = "none";
   };
+
+  const [innerWidth, setInnerWidth] = useState(0);
+  useEffect(() => {
+    setInnerWidth(window.innerWidth);
+  }, []);
+
   return (
     <div>
       <div
         className="hovered-state"
         ref={hoverdState}
         style={{
-          left: mouseX - 300,
+          left: mouseX - (innerWidth / 2 - 450),
           top: mouseY - 250,
         }}
       ></div>
